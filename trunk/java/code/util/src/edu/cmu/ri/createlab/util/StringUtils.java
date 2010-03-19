232afbd9f220bdeedbbc34c8277781bf0aab6cd4
garthabrindoid <garthabrindoid@0fe230e2-d540-11de-8184-f791cb96e2ed>
2010-03-19 15:23:28 +0000

    Added StringUtils class

---
+++ trunk/java/code/util/src/edu/cmu/ri/createlab/util/StringUtils.java
@@ -0,0 +1,41 @@
+package edu.cmu.ri.createlab.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * <p>
+ * <code>StringUtils</code> provides some convenience methods for dealing with {@link String}s.
+ * </p>
+ *
+ * @author Chris Bartley (bartley@cmu.edu)
+ */
+public final class StringUtils
+   {
+   private static final Log LOG = LogFactory.getLog(StringUtils.class);
+
+   /** Converts the given {@link String} to a {@link Double}, or returns <code>null</code> if the conversion failed. */
+   private static Double convertStringToDouble(final String str)
+      {
+      if (str != null && !"".equals(str))
+         {
+         try
+            {
+            return Double.parseDouble(str);
+            }
+         catch (NumberFormatException e)
+            {
+            if (LOG.isErrorEnabled())
+               {
+               LOG.error("NumberFormatException while trying to parse string [" + str + "] as a double", e);
+               }
+            }
+         }
+      return null;
+      }
+
+   private StringUtils()
+      {
+      // private to prevent instantiation
+      }
+   }
