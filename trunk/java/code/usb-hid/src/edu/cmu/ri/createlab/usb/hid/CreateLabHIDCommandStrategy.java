e48ac5d15740d37144b6bf2b8f3ab8aa96de32e7
garthabrindoid <garthabrindoid@0fe230e2-d540-11de-8184-f791cb96e2ed>
2010-07-02 20:30:01 +0000

    added HIDCommandExecutionQueue and its associated classes

---
+++ trunk/java/code/usb-hid/src/edu/cmu/ri/createlab/usb/hid/CreateLabHIDCommandStrategy.java
@@ -0,0 +1,109 @@
+package edu.cmu.ri.createlab.usb.hid;
+
+import edu.cmu.ri.createlab.util.ByteUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * <code>CreateLabHIDCommandStrategy</code> is an abstract {@link HIDCommandStrategy} used for talking with CREATE Lab
+ * HID devices.
+ *
+ * @author Chris Bartley (bartley@cmu.edu)
+ */
+public abstract class CreateLabHIDCommandStrategy implements HIDCommandStrategy
+   {
+   private static final Log LOG = LogFactory.getLog(CreateLabHIDCommandStrategy.class);
+   private static final int TIMEOUT_IN_NANOSECONDS = 1000000000;
+
+   public final HIDCommandResult execute(final HIDDevice hidDevice)
+      {
+      LOG.debug("CreateLabHIDCommandStrategy.execute()");
+
+      // get the command to be written
+      final byte[] command = getCommand();
+
+      // write the command
+      final HIDWriteStatus writeStatus = hidDevice.write(command);
+
+      if (!writeStatus.wasSuccessful())
+         {
+         if (LOG.isErrorEnabled())
+            {
+            LOG.error("CreateLabHIDCommandStrategy.execute(): Number of bytes written [" + writeStatus.getNumBytesActuallyWritten() + "] does not match number of bytes in the command [" + writeStatus.getNumBytesRequestedToWrite() + "].  Command ID = [" + writeStatus.getCommandId() + "]");
+            }
+         }
+
+      // don't bother to try to read unless the command ID is non-null (meaning we actually tried a write)
+      boolean readWasSuccessful = false;
+      byte[] dataRead = null;
+      if (writeStatus.getCommandId() == null)
+         {
+         LOG.debug("CreateLabHIDCommandStrategy.execute(): command ID returned by the write command was null, so we won't attempt to read");
+         }
+      else
+         {
+         // now read until we get the response with the matching command ID, or until we timeout
+         final long startTime = System.nanoTime();
+         final long endTime = startTime + TIMEOUT_IN_NANOSECONDS;
+         int numReads = 0;
+
+         do
+            {
+            final byte[] data = hidDevice.read();
+            numReads++;
+
+            // check the data (the length must be at least two since the first byte is the report ID and the last is the command ID)
+            if (data == null || data.length < 2)
+               {
+               LOG.error("CreateLabHIDCommandStrategy.execute(): data read is null or empty, ignoring read");
+               continue;
+               }
+
+            // the last element of the read data is the command ID
+            final int returnedCommandId = ByteUtils.unsignedByteToInt(data[data.length - 1]);
+
+            // see if we found our desired command ID
+            if (writeStatus.getCommandId() == returnedCommandId)
+               {
+               // we found our response!
+               readWasSuccessful = true;
+
+               // the returned data array from an HID device is a fixed size, but a command will probably only care about
+               // a subset of the bytes.  Do a copy of the bytes we care about, being careful about AIOOBEs.
+               final int numBytesToCopy = Math.min(getSizeOfExpectedResponse(), data.length - 2);
+               if (LOG.isWarnEnabled())
+                  {
+                  if (numBytesToCopy != getSizeOfExpectedResponse())
+                     {
+                     LOG.warn("CreateLabHIDCommandStrategy.execute(): size of expected response [" + getSizeOfExpectedResponse() + "] does not match num bytes we're actually allowed to copy [" + numBytesToCopy + "]");
+                     }
+                  }
+               dataRead = new byte[numBytesToCopy];
+               System.arraycopy(data, 1, dataRead, 0, numBytesToCopy);
+
+               if (LOG.isTraceEnabled())
+                  {
+                  final long currentTime = System.nanoTime();
+                  final double elapsedTimeInMillis = (double)(currentTime - startTime) / 1000000.0;
+                  LOG.trace("CreateLabHIDCommandStrategy.execute(): read successful -- it took [" + numReads + "] reads and [" + elapsedTimeInMillis + "] ms to find the response to the write command.");
+                  }
+               }
+            else
+               {
+               if (LOG.isErrorEnabled())
+                  {
+                  LOG.error("CreateLabHIDCommandStrategy.execute(): unexpected command ID in the data read.  Found [" + returnedCommandId + "], was expecting [" + writeStatus.getCommandId() + "]");
+                  }
+               }
+            }
+         while (!readWasSuccessful && System.nanoTime() < endTime);
+         }
+
+      return new HIDCommandResult(writeStatus.wasSuccessful(), readWasSuccessful, dataRead);
+      }
+
+   protected abstract byte[] getCommand();
+
+   /** Returns the number of bytes of the expected response. */
+   protected abstract int getSizeOfExpectedResponse();
+   }
---
+++ trunk/java/code/usb-hid/src/edu/cmu/ri/createlab/usb/hid/HIDCommandExecutionQueue.java
@@ -0,0 +1,151 @@
+package edu.cmu.ri.createlab.usb.hid;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
+import edu.cmu.ri.createlab.util.thread.DaemonThreadFactory;
+import org.apache.commons.lang.NotImplementedException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * The HIDCommandExecutionQueue serializes communication commands with an HID Device to ensure that they are
+ * executed in the order received, without the possibility of one command's inputs or response conflicting with
+ * another's.
+ *
+ * @author Chris Bartley (bartley@cmu.edu)
+ */
+public final class HIDCommandExecutionQueue
+   {
+   private static final Log LOG = LogFactory.getLog(HIDCommandExecutionQueue.class);
+
+   public static HIDCommandExecutionQueue create(final short usbVendorId, final short usbProductId) throws NotImplementedException
+      {
+      // create the HID device
+      if (LOG.isDebugEnabled())
+         {
+         LOG.debug("HIDCommandExecutionQueue.create(): creating HID device for vendor ID [" + usbVendorId + "] and product ID [" + usbProductId + "]");
+         }
+      final HIDDevice hidDevice = HIDDeviceFactory.create(usbVendorId, usbProductId);
+
+      LOG.debug("HIDCommandExecutionQueue.create(): attempting connection...");
+      if (hidDevice.connect())
+         {
+         LOG.debug("HIDCommandExecutionQueue.create(): connection successful!");
+         return new HIDCommandExecutionQueue(hidDevice);
+         }
+      else
+         {
+         LOG.debug("HIDCommandExecutionQueue.create(): connection failed, returning null");
+         }
+
+      return null;
+      }
+
+   private final HIDDevice hidDevice;
+   private final ExecutorService executor = Executors.newSingleThreadExecutor(new DaemonThreadFactory("HIDCommandExecutionQueue.executor"));
+
+   private HIDCommandExecutionQueue(final HIDDevice hidDevice)
+      {
+      this.hidDevice = hidDevice;
+      }
+
+   /**
+    * Adds the given {@link HIDCommandStrategy} to the queue, blocks until its execution is complete, and then returns the
+    * result.  Returns <code>null</code> if an error occurred while trying to obtain the result.
+    */
+   public HIDCommandResult execute(final HIDCommandStrategy commandStrategy)
+      {
+      LOG.trace("HIDCommandExecutionQueue.execute()");
+
+      // create the command
+      final HIDCommand command = new HIDCommand(commandStrategy, hidDevice);
+
+      // create the future task
+      final FutureTask<HIDCommandResult> task = new FutureTask<HIDCommandResult>(command);
+
+      try
+         {
+         // execute the task
+         LOG.trace("HIDCommandExecutionQueue.execute():   Calling execute()");
+         executor.execute(task);
+
+         // block and wait for the return value
+         LOG.trace("HIDCommandExecutionQueue.execute():   Calling get() and returning response");
+         return task.get();
+         }
+      catch (RejectedExecutionException e)
+         {
+         LOG.error("HIDCommandExecutionQueue.execute():RejectedExecutionException while trying to schedule the command for execution", e);
+         }
+      catch (InterruptedException e)
+         {
+         LOG.error("HIDCommandExecutionQueue.execute():InterruptedException while trying to get the HIDCommandResult", e);
+         }
+      catch (ExecutionException e)
+         {
+         LOG.error("HIDCommandExecutionQueue.execute():ExecutionException while trying to get the HIDCommandResult [" + e.getCause() + "]", e);
+         }
+
+      LOG.trace("HIDCommandExecutionQueue.execute():   Returning null response");
+      return null;
+      }
+
+   /**
+    * Adds the given {@link HIDCommandStrategy} to the queue, blocks until its execution is complete, and then returns only
+    * the status of the result.  This is merely a convenience method which delegates to {@link #execute(HIDCommandStrategy
+    * commandStrategy)} for cases where you only need the result status of the command to be executed.
+    *
+    * @see #execute(HIDCommandStrategy commandStrategy)
+    */
+   public boolean executeAndReturnStatus(final HIDCommandStrategy commandStrategy)
+      {
+      final HIDCommandResult response = execute(commandStrategy);
+
+      return response != null && response.wasSuccessful();
+      }
+
+   /**
+    * Shuts down the command queue and then closes the HID device.  Commands in the queue are allowed to execute before
+    * shutdown, but no new commands will be accepted.
+    */
+   public void shutdown()
+      {
+      // shut down the command queue
+      try
+         {
+         LOG.debug("HIDCommandExecutionQueue.shutdown(): Shutting down the HID device command execution queue");
+         final List<Runnable> unexecutedTasks = executor.shutdownNow();
+         LOG.debug("HIDCommandExecutionQueue.shutdown(): Unexecuted tasks: " + (unexecutedTasks == null ? 0 : unexecutedTasks.size()));
+         LOG.debug("HIDCommandExecutionQueue.shutdown(): Waiting for the HID device command execution queue to shutdown.");
+         executor.awaitTermination(10, TimeUnit.SECONDS);
+         LOG.debug("HIDCommandExecutionQueue.shutdown(): HID device command execution queue successfully shutdown");
+         }
+      catch (Exception e)
+         {
+         LOG.error("HIDCommandExecutionQueue.shutdown(): Exception while trying to shut down the HID device command execution queue", e);
+         }
+
+      // disconnect from the HID device
+      try
+         {
+         LOG.debug("HIDCommandExecutionQueue.shutdown(): Now attempting to disconnect from the HID device...");
+         if (hidDevice.disconnect())
+            {
+            LOG.debug("HIDCommandExecutionQueue.shutdown(): HID device disconnected successfully.");
+            }
+         else
+            {
+            LOG.debug("HIDCommandExecutionQueue.shutdown(): Failed to disconnect from the HID device.");
+            }
+         }
+      catch (Exception e)
+         {
+         LOG.error("HIDCommandExecutionQueue.shutdown(): Exception while trying to disconnect from the HID device", e);
+         }
+      }
+   }
