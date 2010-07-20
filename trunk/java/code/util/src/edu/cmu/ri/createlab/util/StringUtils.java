2065744a752bdc3e82940443fc0aa8aa8f897a16
garthabrindoid <garthabrindoid@0fe230e2-d540-11de-8184-f791cb96e2ed>
2010-07-20 17:17:15 +0000

    added convertStringToInteger()

--- trunk/java/code/util/src/edu/cmu/ri/createlab/util/StringUtils.java
+++ trunk/java/code/util/src/edu/cmu/ri/createlab/util/StringUtils.java
@@ -27,7 +27,27 @@ public final class StringUtils
             {
             if (LOG.isErrorEnabled())
                {
-               LOG.error("NumberFormatException while trying to parse string [" + str + "] as a double", e);
+               LOG.error("NumberFormatException while trying to parse string [" + str + "] as a Double", e);
+               }
+            }
+         }
+      return null;
+      }
+
+   /** Converts the given {@link String} to a {@link Integer}, or returns <code>null</code> if the conversion failed. */
+   public static Integer convertStringToInteger(final String str)
+      {
+      if (str != null && !"".equals(str))
+         {
+         try
+            {
+            return Integer.parseInt(str);
+            }
+         catch (NumberFormatException e)
+            {
+            if (LOG.isErrorEnabled())
+               {
+               LOG.error("NumberFormatException while trying to parse string [" + str + "] as an Integer", e);
                }
             }
          }
