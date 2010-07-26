d0382ab6ed9c43d2deb061d8fbd5f09032233291
garthabrindoid@gmail.com <garthabrindoid@gmail.com@0fe230e2-d540-11de-8184-f791cb96e2ed>
2010-07-26 14:37:45 +0000

    Added LifecycleManager

---
+++ java/code/util/src/edu/cmu/ri/createlab/util/runtime/LifecycleManager.java
@@ -0,0 +1,11 @@
+package edu.cmu.ri.createlab.util.runtime;
+
+/**
+ * @author Chris Bartley (bartley@cmu.edu)
+ */
+public interface LifecycleManager
+   {
+   void startup();
+
+   void shutdown();
+   }
