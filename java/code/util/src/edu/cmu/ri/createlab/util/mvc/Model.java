ae55f365116df440ca9bb47b2f4da7f364ff500b
garthabrindoid@gmail.com <garthabrindoid@gmail.com@0fe230e2-d540-11de-8184-f791cb96e2ed>
2010-07-26 14:33:50 +0000

    Initial checkin of MVC classes

---
+++ java/code/util/src/edu/cmu/ri/createlab/util/mvc/Model.java
@@ -0,0 +1,53 @@
+package edu.cmu.ri.createlab.util.mvc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import edu.cmu.ri.createlab.util.thread.DaemonThreadFactory;
+
+/**
+ * <p>
+ * <code>Model</code> provides common functionality for MVC model classes.
+ * </p>
+ *
+ * @author Chris Bartley (bartley@cmu.edu)
+ */
+public abstract class Model<T, U>
+   {
+   private List<EventListener<U>> eventListeners = new ArrayList<EventListener<U>>();
+   private ExecutorService executorService = Executors.newSingleThreadExecutor(new DaemonThreadFactory(this.getClass().getSimpleName()));
+
+   public final synchronized void addEventListener(final EventListener<U> listener)
+      {
+      if (listener != null)
+         {
+         eventListeners.add(listener);
+         }
+      }
+
+   /**
+    * Publishes the given object to all registered {@link EventListener}s.  Publication is performed in a separate
+    * thread so that control can quickly be returned to the caller.  Published objects are guaranteed to be received by
+    * the listeners in the same order in which they were published.
+    */
+   protected final synchronized void publishEventToListeners(final U obj)
+      {
+      if (!eventListeners.isEmpty())
+         {
+         executorService.execute(
+               new Runnable()
+               {
+               public void run()
+                  {
+                  for (final EventListener<U> listener : eventListeners)
+                     {
+                     listener.handleEvent(obj);
+                     }
+                  }
+               });
+         }
+      }
+
+   public abstract U update(final T data);
+   }
---
+++ java/code/util/src/edu/cmu/ri/createlab/util/mvc/View.java
@@ -0,0 +1,38 @@
+package edu.cmu.ri.createlab.util.mvc;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * <p>
+ * <code>View</code> provides common functionality for MVC view classes.
+ * </p>
+ *
+ * @author Chris Bartley (bartley@cmu.edu)
+ */
+public abstract class View<T> implements EventListener<T>
+   {
+   public final void handleEvent(final T eventData)
+      {
+      if (SwingUtilities.isEventDispatchThread())
+         {
+         handleEventInGUIThread(eventData);
+         }
+      else
+         {
+         SwingUtilities.invokeLater(
+               new Runnable()
+               {
+               public void run()
+                  {
+                  handleEventInGUIThread(eventData);
+                  }
+               });
+         }
+      }
+
+   /**
+    * Method for handling the event, guaranteed to run in the GUI thread.
+    */
+   protected abstract void handleEventInGUIThread(final T eventData);
+   }
+
