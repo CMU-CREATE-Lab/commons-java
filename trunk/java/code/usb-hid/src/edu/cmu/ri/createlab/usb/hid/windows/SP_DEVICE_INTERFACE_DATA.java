9edf0955c9c379699344023af1e3f07787697dbe
garthabrindoid <garthabrindoid@0fe230e2-d540-11de-8184-f791cb96e2ed>
2010-08-06 21:27:26 +0000

    Changes required for 64-bit Windows compatibility

--- trunk/java/code/usb-hid/src/edu/cmu/ri/createlab/usb/hid/windows/SP_DEVICE_INTERFACE_DATA.java
+++ trunk/java/code/usb-hid/src/edu/cmu/ri/createlab/usb/hid/windows/SP_DEVICE_INTERFACE_DATA.java
@@ -1,5 +1,6 @@
 package edu.cmu.ri.createlab.usb.hid.windows;
 
+import com.sun.jna.Pointer;
 import com.sun.jna.Structure;
 
 /**
@@ -12,5 +13,11 @@ public final class SP_DEVICE_INTERFACE_DATA extends Structure
    public int cbSize;
    public GUID interfaceClassGuid;
    public int flags;
-   public int reserved;
+
+   // Making this a Pointer was a total guess.  I originally had it as an int, but 64-bit Windows 7 (running 64-bit
+   // Java) barfed on it.  Then I changed it to a long, which made 64-bit Windows 7 happy, but then that broke 32-bit
+   // Windows XP.  I then tried NativeLong, which I expected to do the right thing, but that only worked on 32-bit
+   // Windows XP because--for some reason I can't explain--NativeLong.SIZE is 4 on both 32-bit and 64-bit Windows.  So,
+   // I tried Pointer, and--amazingly--that works in both 32-bit and 64-bit Windows.
+   public Pointer reserved;
    }
