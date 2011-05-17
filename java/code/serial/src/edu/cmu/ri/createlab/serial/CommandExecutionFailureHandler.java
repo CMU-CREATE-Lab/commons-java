9aff0b7add610ca9aba2a759bd1340fa644515fa
garthabrindoid <garthabrindoid@0fe230e2-d540-11de-8184-f791cb96e2ed>
2011-05-17 16:50:08 +0000

    added support for variable-length responses, added ReturnValueCommandExecutor and NoReturnValueCommandExecutor, and convertResponse() must now be implemented by subclasses of SerialPortReturnValueCommandStrategy.

---
+++ java/code/serial/src/edu/cmu/ri/createlab/serial/CommandExecutionFailureHandler.java
@@ -0,0 +1,9 @@
+package edu.cmu.ri.createlab.serial;
+
+/**
+ * @author Chris Bartley (bartley@cmu.edu)
+ */
+public interface CommandExecutionFailureHandler
+   {
+   void handleExecutionFailure();
+   }
