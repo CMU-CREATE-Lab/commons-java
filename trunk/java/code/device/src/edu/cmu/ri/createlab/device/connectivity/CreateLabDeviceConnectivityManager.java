91d655e1ff684bf8c316c05b9843de7bb27d2065
garthabrindoid <garthabrindoid@0fe230e2-d540-11de-8184-f791cb96e2ed>
2011-06-01 19:17:09 +0000

    Added a connect() method to CreateLabDeviceConnectivityManager.  This method blocks until a connection is established (unlike scanAndConnect() which returns immediately while the connecting is done in the background)

--- trunk/java/code/device/src/edu/cmu/ri/createlab/device/connectivity/CreateLabDeviceConnectivityManager.java
+++ trunk/java/code/device/src/edu/cmu/ri/createlab/device/connectivity/CreateLabDeviceConnectivityManager.java
@@ -1,17 +1,27 @@
 package edu.cmu.ri.createlab.device.connectivity;
 
+import edu.cmu.ri.createlab.device.CreateLabDeviceProxy;
 import edu.cmu.ri.createlab.device.CreateLabDeviceProxyProvider;
 
 /**
  * @author Chris Bartley (bartley@cmu.edu)
  */
-public interface CreateLabDeviceConnectivityManager extends CreateLabDeviceProxyProvider, CreateLabDeviceConnectionEventPublisher
+public interface CreateLabDeviceConnectivityManager<ProxyClass extends CreateLabDeviceProxy> extends CreateLabDeviceProxyProvider<ProxyClass>, CreateLabDeviceConnectionEventPublisher
    {
-   /** Periodically scans and connects to the first target device it finds. */
+   /**
+    * Scans and connects to the first target device it finds. This method blocks until a connection is established.  A
+    * {@link ConnectionException} will only be thrown if some unrecoverable failure arises.
+    */
+   ProxyClass connect() throws ConnectionException;
+
+   /**
+    * Scans and connects to the first target device it finds. This method returns immediately because the scanning is
+    * done in a background thread.
+    */
    void scanAndConnect();
 
-   /** Cancels the scan, or does nothing if not scanning. */
-   void cancelScanning();
+   /** Cancels the connection attemp, or does nothing if not scanning. */
+   void cancelConnecting();
 
    /** Disconnects from the device. */
    void disconnect();
