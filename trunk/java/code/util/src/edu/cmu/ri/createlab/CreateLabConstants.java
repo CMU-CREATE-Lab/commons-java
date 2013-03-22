fc049a32c8a04fbb0a694b02e42d20fc1d903c5c
bartley@cmu.edu <bartley@cmu.edu@0fe230e2-d540-11de-8184-f791cb96e2ed>
2013-03-22 20:09:35 +0000

    Added ability to override the CREATE Lab home directory by defining the "CreateLabHomeDirectory" system property.  Fixes Visual Programmer issue 145 (https://code.google.com/p/create-lab-visual-programmer/issues/detail?id=145)

--- trunk/java/code/util/src/edu/cmu/ri/createlab/CreateLabConstants.java
+++ trunk/java/code/util/src/edu/cmu/ri/createlab/CreateLabConstants.java
@@ -11,9 +11,16 @@ import java.io.File;
  */
 public final class CreateLabConstants
    {
+   /**
+    * File path constants common to CREATE Lab applications.  The CREATE Lab home directory defaults to a directory
+    * named <code>CREATELab</code> under the user's home directory, but can be overridden by defining the
+    * <code>CreateLabHomeDirectory</code> system property and setting it to the desired directory's absolute path.
+    */
    public static final class FilePaths
       {
-      private static final String CREATE_LAB_HOME_PATH = System.getProperty("user.home") + File.separator + "CREATELab" + File.separator;
+      public static final String CREATE_LAB_HOME_SYSTEM_PROPERTY_NAME = "CreateLabHomeDirectory";
+      private static final String DEFAULT_CREATE_LAB_HOME_PATH = System.getProperty("user.home") + File.separator + "CREATELab" + File.separator;
+      private static final String CREATE_LAB_HOME_PATH = System.getProperty(CREATE_LAB_HOME_SYSTEM_PROPERTY_NAME, DEFAULT_CREATE_LAB_HOME_PATH);
       public static final File CREATE_LAB_HOME_DIR = new File(CREATE_LAB_HOME_PATH);
       public static final File AUDIO_DIR = new File(CREATE_LAB_HOME_DIR, "Audio");
 
